7b00ebb7a348f5b2257d6fc80ffad4cfce267855
Xnuk Shuman <admin@xnu.kr>
2020-04-13 17:35:53 +0900

    path support

--- main.rs
+++ main.rs
@@ -3,16 +3,35 @@ use serde_json::value::Value;
 use serde_json::ser::to_string_pretty;
 use std::io::{Read, Write, stdin, stdout};
 use std::process::exit;
+use std::env::args_os;
+use std::fs::File;
+use core::fmt::Display;
 
-fn main() {
-	let json = to_string_pretty(&from_str::<Value>(&{
-		let mut buf = String::new();
-		stdin().read_to_string(&mut buf).unwrap();
-		buf
-	}).unwrap_or_else(|e| {
+fn unwrap<'a, T, E>(x: &'a Result<T, E>) -> &'a T where E: Display {
+	x.as_ref().unwrap_or_else(|e| {
 		eprintln!("{}", e);
 		exit(1);
-	})).unwrap();
+	})
+}
 
-	stdout().write_all(json.as_bytes()).unwrap();
+fn main() -> std::io::Result<()> {
+	let path = args_os().nth(1);
+
+	let mut buf = String::new();
+
+	match path {
+		Some(p) => {
+			let a = File::open(p);
+			unwrap(&a).read_to_string(&mut buf)?
+		},
+		None => stdin().read_to_string(&mut buf)?,
+	};
+
+	let json = {
+		let a = from_str::<Value>(&buf);
+		to_string_pretty(unwrap(&a)).unwrap()
+	};
+
+
+	stdout().write_all(json.as_bytes())
 }
