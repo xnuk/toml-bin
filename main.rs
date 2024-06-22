d3825d780be512835ce03c32213daa4ad4028783
Xnuk Shuman <admin@xnu.kr>
2024-06-22 15:34:46 +0900

    greenhousing code + add formatter rule

--- main.rs
+++ main.rs
@@ -1,10 +1,13 @@
-use std::io::{Read, Write, stdin, stdout};
-use std::process::exit;
+use core::fmt::Display;
 use std::env::args_os;
 use std::fs::File;
-use core::fmt::Display;
+use std::io::{self, Read, Write};
+use std::process::exit;
 
-fn unwrap<'a, T, E>(x: &'a Result<T, E>) -> &'a T where E: Display {
+fn unwrap<T, E>(x: &Result<T, E>) -> &T
+where
+	E: Display,
+{
 	x.as_ref().unwrap_or_else(|e| {
 		eprintln!("{}", e);
 		exit(1);
@@ -13,36 +16,36 @@ fn unwrap<'a, T, E>(x: &'a Result<T, E>) -> &'a T where E: Display {
 
 macro_rules! pretty {
 	($from:tt -> $to:tt , $x:expr) => {
-		$to::ser::to_string_pretty(
-			unwrap(&{
-				$from::de::from_str::<$to::value::Value>($x)
-			})
-		).expect("")
-	}
+		$to::ser::to_string_pretty(unwrap(&{
+			$from::de::from_str::<$to::value::Value>($x)
+		}))
+		.expect("")
+	};
 }
 
-fn run() -> std::io::Result<()> {
+fn run() -> io::Result<()> {
 	let mut buf = String::new();
 
-	let path = args_os().nth(1).filter(|path| { path != "-" });
+	let path = args_os().nth(1).filter(|path| path != "-");
 
 	match path {
-		Some(path) => {
-			File::open(path)?.read_to_string(&mut buf)
-		},
-		None => stdin().read_to_string(&mut buf),
+		Some(path) => File::open(path)?.read_to_string(&mut buf),
+		None => io::stdin().read_to_string(&mut buf),
 	}?;
 
 	let buf = buf.trim_start();
-	let is_json = buf.chars().next() == Some('{');
+	let is_json = buf.starts_with('{');
 
-	stdout().write_all(
+	io::stdout().write_all(
 		if is_json {
 			pretty!(serde_json -> toml, &buf)
 		} else {
 			pretty!(toml -> serde_json, &buf)
-		}.as_bytes()
+		}
+		.as_bytes(),
 	)
 }
 
-fn main() { *unwrap(&run()) }
+fn main() {
+	*unwrap(&run())
+}
